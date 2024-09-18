802edfef677f349d6105f66d27e1ee6c3413cf29
Carlos Santos <cadu.vs@hotmail.com>
2024-09-18 16:02:20 -0300

    feat: database is created on app launch

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./components/HomeScreen";
+import { createTables } from "./services/Database";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  createTables(); // inicializa o banco de dados
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
