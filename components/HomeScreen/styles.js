db4baac7f994299d3f21cee80002745d7220a09b
Carlos Santos <cadu.vs@hotmail.com>
2024-09-18 15:06:37 -0300

    feat: early navigation system

---
+++ components/HomeScreen/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#252c49",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 40,
+  },
+});
+
+export default styles;
