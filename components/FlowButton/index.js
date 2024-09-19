90535fdb9b25e751c8c305bce3ac6225b91127d5
Carlos Santos <cadu.vs@hotmail.com>
2024-09-18 22:23:58 -0300

    style: formatted flow button

--- components/FlowButton/index.js
+++ components/FlowButton/index.js
@@ -1,9 +1,16 @@
 import { TouchableOpacity, Text } from "react-native";
 import styles from "./styles";
 
-export default function FlowButton({ text, onPress, backgroundColor = "#219de2" }) {
+export default function FlowButton({
+  text,
+  onPress,
+  backgroundColor = "#219de2",
+}) {
   return (
-    <TouchableOpacity style={[styles.button, { "backgroundColor": backgroundColor }]} onPress={onPress}>
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor: backgroundColor }]}
+      onPress={onPress}
+    >
       <Text style={styles.buttonLabel}>{text}</Text>
     </TouchableOpacity>
   );
